dcb294449dcb2abc19d890e43aeec2e67e0cb2f9
nurrahmadind <dinda.smkn6@gmail.com>
2025-11-17 13:41:01 +0700

    Membuat Scene MainMenu, Credits, GameOver, dan YouWin
    Membuat scene
    1. MainMenu
    2. Credits
    3. GameOver
    4. YouWin
    
    sudah dapat dijalankan
    
    untuk main lagi nanti akan ke Scene GamePlay

---
+++ Assets/Game/Scripts/LoseWin.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class LoseWin : MonoBehaviour
+{
+    
+    public TMP_Text objectText;// Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void MainLagi()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void ExitGame()
+    {
+        Debug.Log("Keluar dari Game");
+        
+        // Perintah standar untuk keluar dari aplikasi. 
+        // Ini hanya akan bekerja saat game sudah di-build (file .exe).
+        Application.Quit();
+
+        // Optional: Baris di bawah ini hanya untuk membuat tombol Quit berfungsi
+        // saat Anda mengujinya di Unity Editor. Hapus baris ini saat di-build.
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}    
